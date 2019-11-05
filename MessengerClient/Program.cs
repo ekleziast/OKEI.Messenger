8b933d3e3b301962728fdedfb705b36fc51eef37
ekleziast <rami1@mail.ru>
2019-11-06 00:50:26 +0500

    Server and Client was created

---
+++ MessengerClient/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerClient
+{
+    class Program
+    {
+        public static readonly int SERVERPORT = 8005;
+        public static readonly IPAddress SERVERADDRESS = IPAddress.Parse("127.0.0.1");
+        static Socket listeningSocket; // Сокет
+        static void Main(string[] args)
+        {
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen); // Создание потока
+                listeningTask.Start(); // Запуск потока
+
+                while (true) // Отправление сообщений серверу в бесконечном цикле
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(SERVERADDRESS, SERVERPORT);
+                    listeningSocket.SendTo(data, remotePoint);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 0);
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
+
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Закрывает соединение
+        /// </summary>
+        private static void Close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+            Console.WriteLine("Сервер остановлен!");
+        }
+    }
+}
---
+++ MessengerServer/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerServer
+{
+    class Program
+    {
+        public static readonly int PORT = 8005;
+
+        static Socket listeningSocket; // Сокет
+        static List<IPEndPoint> Clients = new List<IPEndPoint>(); // Список клиентов
+        static void Main(string[] args)
+        {
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen); // Создание потока для получения сообщений
+                listeningTask.Start();
+                listeningTask.Wait(); // Не идем дальше пока поток не будет остановлен
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        private static void Listen()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), PORT);
+                listeningSocket.Bind(localIP);
+
+                Console.WriteLine("Сервер запущен! Ожидаем подключения...");
+
+                while (true)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+                    EndPoint remoteIP = new IPEndPoint(IPAddress.Any, 0); // адрес удаленного подключения
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIP);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (listeningSocket.Available > 0);
+
+                    IPEndPoint remoteFullIP = remoteIP as IPEndPoint; // данные о подключении
+                    Console.WriteLine(
+                        $"({DateTime.Now.ToShortTimeString()}) {remoteFullIP.Address.ToString()}:{remoteFullIP.Port} - " +
+                        $"{builder.ToString()}"); // Logging
+
+                    bool isClientInCollection = Clients.Any(o => 
+                        o.Address.ToString() == remoteFullIP.Address.ToString()
+                        && o.Port == remoteFullIP.Port
+                    );
+                    if (!isClientInCollection) Clients.Add(remoteFullIP); // Добавляем нового клиента в список
+
+                    // TODO: Оповещение клиентов о получении нового сообщения
+                    // Сейчас происходит отправка всем клиентов
+                    BroadcastMessage(builder.ToString(), remoteFullIP);
+
+                }
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Отправляет сообщение всем подключенным клиентам
+        /// </summary>
+        /// <param name="message">Текст сообщения</param>
+        /// <param name="ip">IPEndPoint отправителя</param>
+        private static void BroadcastMessage(string message, IPEndPoint ip)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            Clients.ForEach(o => {
+            if ($"{o.Address.ToString()}:{o.Port}" != $"{ip.Address.ToString()}:{ip.Port}")
+                {
+                    listeningSocket.SendTo(data, o);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Закрывает соединение
+        /// </summary>
+        private static void Close()
+        {
+            if(listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+            Console.WriteLine("Сервер остановлен!");
+        }
+    }
+}
