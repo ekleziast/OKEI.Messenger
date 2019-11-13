31ba84ff9f00f0281adf761504218038e6ebf045
ekleziast <rami1@mail.ru>
2019-11-13 14:24:35 +0500

    Структуры запросов и коды сообщений

---
+++ ContextLibrary/Model/Codes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextLibrary
+{
+    /// <summary>
+    /// Коды запросов и ответов на сервер
+    /// </summary>
+    public enum Codes : int
+    {
+        False = 0,
+        True = 1,
+        /// <summary>
+        /// Подтверждение
+        /// </summary>
+        Confirmation = 2,
+        /// <summary>
+        /// Регистрация
+        /// </summary>
+        Registraion = 3,
+        /// <summary>
+        /// Авторизация
+        /// </summary>
+        Authorization = 4,
+        /// <summary>
+        /// Новое сообщение
+        /// </summary>
+        NewMessage = 5,
+        /// <summary>
+        /// Новый диалог
+        /// </summary>
+        NewConversation = 6,
+        /// <summary>
+        /// В диалог добавлен новый участник
+        /// </summary>
+        NewMember = 7,
+        /// <summary>
+        /// Из диалога удален участник
+        /// </summary>
+        RemoveMember = 8,
+        /// <summary>
+        /// Участник покинул диалог
+        /// </summary>
+        LeaveMember = 9,
+        /// <summary>
+        /// Клиент разлогинился
+        /// </summary>
+        LogOut = 10,
+    }
+}
---
+++ ContextLibrary/Model/DefaultJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextLibrary
+{
+    public class DefaultJSON
+    {
+        public int Code { get; set; }
+        public string Content { get; set; }
+    }
+}
